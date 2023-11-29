ce1628e929dc800cee51fdaf88d701d96138e1eb
Givforks <givens.abraham@live.com>
2023-11-28 16:26:21 -0800

    python

---
+++ 0x00-python-hello_world/5-print_string.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+str = "Givforks Project"
+print(str + str + str)
+print(str[:9])
---
+++ 0x00-python-hello_world/6-concat.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str1 = "Givforks"
+str2 = "Project"
+str1 += (" " + str2)
+print("Welcome to {}!".format(str1))

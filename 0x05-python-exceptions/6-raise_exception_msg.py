ba99fe622064bb53c7a3b67e575c601889eb4966
Givforks <givens.abraham@live.com>
2023-12-18 22:31:30 -0800

    python-exceptions

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 6-raise_exception_msg.py
+# Givens Emmah Abraham <givens.abraham@live.com>
+
+
+def raise_exception_msg(message=""):
+    """Raise a NameError exception with a message."""
+    raise NameError(message)
---
+++ 0x05-python-exceptions/README.md
@@ -0,0 +1,2 @@
+i learnt how to mannage error
+0x05. Python - Exceptions

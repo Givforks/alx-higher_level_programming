d052c4c5e3f811a53a3f8c962564845402328f91
Givforks <givens.abraham@live.com>
2024-01-06 17:30:43 -0800

    0x08-python-more_classes

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 0-rectangle.py
+# Givens Emmah Abraham <givens.abraham@live.com>
+"""Defines a Rectangle class."""
+
+
+class Rectangle:
+    """Represent a rectangle."""
+    pass

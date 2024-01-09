416a6f108e6777ee404df76d545817fccea0b070
Givforks <givens.abraham@live.com>
2024-01-08 17:15:12 -0800

    python-inheritance

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 5-base_geometry.py
+# Givens Emmah Abraham <givens.abraham@live.com>
+"""Defines an empty class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+    pass
